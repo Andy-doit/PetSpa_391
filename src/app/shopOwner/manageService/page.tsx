da4a53766fefbb3efd8d510bc26846e1b2d9e819
duyhoangto <hoangtdse170030@fpt.edu.vn>
2024-06-09 21:29:42 +0700

    modal upate,delete,view detail

--- src/app/shopOwner/manageService/page.tsx
+++ src/app/shopOwner/manageService/page.tsx
@@ -3,16 +3,50 @@ import { FcPlus } from "react-icons/fc";
 import { useState } from "react";
 import ServiceManagement from "@/components/tableServiceofShop/page";
 import ModalCreateService from "@/components/modalCreateService/page";
-
+import ModalViewServiceProps from "@/components/modalViewDetailService/page";
+import ModalUpdateServiceProps from "@/components/modalUpdateSerivce/page";
+import ModalDeleteServiceProps from "@/components/modalDeleteService/page"
 const ManageService = () => {
     const [showModalCreateService, setShowModalCreateService] = useState(false);
+    const [showModalViewService, setShowModalViewService] = useState(false);
+    const [showModalUpdateService, setShowModalUpdateService] = useState(false);
+    const [selectedService, setSelectedService] = useState(null);
+    const [showModalDeleteService, setShowModalDeleteService] = useState(false);
 
     const handleCloseModal = () => {
         setShowModalCreateService(false);
     };
 
+    const handleViewService = (service: any) => {
+        setSelectedService(service);
+        setShowModalViewService(true);
+    };
+
+    const handleCloseViewModal = () => {
+        setShowModalViewService(false);
+        setSelectedService(null);
+    };
+
+    const handleUpdateService = (service: any) => {
+        setSelectedService(service);
+        setShowModalUpdateService(true);
+    };
+
+    const handleCloseUpdateModal = () => {
+        setShowModalUpdateService(false);
+        setSelectedService(null);
+    };
+    const handleDeleteService = (service: any) => {
+        setSelectedService(service);
+        setShowModalDeleteService(true);
+    };
+    const handleCloseDeleteModal = () => {
+        setShowModalDeleteService(false);
+        setSelectedService(null);
+    };
+
     return (
-        <div className="container mx-auto px-4">
+        <div className="mx-auto px-4">
             <div className="flex justify-between items-center mb-4">
                 <h2 className="text-2xl font-bold">Quản Lí Dịch Vụ</h2>
                 <button
@@ -24,10 +58,25 @@ const ManageService = () => {
                 </button>
             </div>
             <div className="table-user-container">
-                <ServiceManagement />
+                <ServiceManagement onViewService={handleViewService} onUpdateService={handleUpdateService} onDeleteService={handleDeleteService} />
                 <ModalCreateService
                     isOpen={showModalCreateService}
                     onClose={handleCloseModal}
+                />
+                <ModalViewServiceProps
+                    isOpen={showModalViewService}
+                    onClose={handleCloseViewModal}
+                    service={selectedService}
+                />
+                <ModalUpdateServiceProps
+                    isOpen={showModalUpdateService}
+                    onClose={handleCloseUpdateModal}
+                    service={selectedService}
+                />
+                <ModalDeleteServiceProps
+                    isOpen={showModalDeleteService}
+                    onClose={handleCloseDeleteModal}
+                    serviceToDelete={selectedService}
                 />
             </div>
         </div>
--- src/components/tableServiceofShop/page.tsx
+++ src/components/tableServiceofShop/page.tsx
@@ -1,29 +1,34 @@
 'use client'
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Button, Input } from "@nextui-org/react";
 import { useState } from "react";
-
-export default function ServiceManagement() {
+import { FaEye, FaEdit, FaTrash } from 'react-icons/fa';
+export default function ServiceManagement({
+    onViewService,
+    onUpdateService,
+    onDeleteService, // Thêm prop onDeleteService
+}: {
+    onViewService: (service: any) => void;
+    onUpdateService: (service: any) => void;
+    onDeleteService: (service: any) => void; // Thêm kiểu cho prop onDeleteService
+}) {
     const [services, setServices] = useState([
-        { id: 1, name: "Dịch vụ tắm rửa", price: 100000, description: "Dịch vụ tắm rửa cho thú cưng" },
-        { id: 2, name: "Dịch vụ cắt tỉa lông", price: 200000, description: "Dịch vụ cắt tỉa lông cho thú cưng" },
-        { id: 3, name: "Dịch vụ khách sạn thú cưng", price: 500000, description: "Dịch vụ lưu trú cho thú cưng" },
-        { id: 4, name: "Dịch vụ mát xa", price: 150000, description: "Dịch vụ mát xa cho thú cưng" },
+        { id: 1, name: "Dịch vụ tắm rửa", price: 100000, type: "dog", description: "Dịch vụ tắm rửa cho thú cưng" },
+        { id: 2, name: "Dịch vụ cắt tỉa lông", price: 200000, type: "cat", description: "Dịch vụ cắt tỉa lông cho thú cưng" },
+        { id: 3, name: "Dịch vụ khách sạn thú cưng", price: 500000, type: "dog", description: "Dịch vụ lưu trú cho thú cưng" },
+        { id: 4, name: "Dịch vụ mát xa", price: 150000, type: "dog", description: "Dịch vụ mát xa cho thú cưng" },
+        { id: 5, name: "Dịch vụ xoa bóp ", price: 221000, type: "cat", description: "Dịch vụ xoa bóp cực pheee cho thú cưng" },
+        { id: 6, name: "Dịch vụ đấm bóp ", price: 421000, type: "dog", description: "Dịch vụ đấm bóp cực chill cho thú cưng" },
+        { id: 7, name: "Dịch vụ vệ sinh răng miệng", price: 281999, type: "cat", description: "Dịch vụ vệ sinh  răng miệng cho thú cưng" },
     ]);
-
-    const [newService, setNewService] = useState({ id: 0, name: "", price: 0, description: "" });
-
-
-
-
 
     return (
         <div>
-
             <Table aria-label="Service Management">
                 <TableHeader>
                     <TableColumn>ID</TableColumn>
                     <TableColumn>Tên dịch vụ</TableColumn>
                     <TableColumn>Giá</TableColumn>
+                    <TableColumn>Loại thú cưng</TableColumn>
                     <TableColumn>Mô tả</TableColumn>
                     <TableColumn>Hành động</TableColumn>
                 </TableHeader>
@@ -33,24 +38,48 @@ export default function ServiceManagement() {
                             <TableCell>{service.id}</TableCell>
                             <TableCell>{service.name}</TableCell>
                             <TableCell>{service.price}</TableCell>
+                            <TableCell>{service.type}</TableCell>
                             <TableCell>{service.description}</TableCell>
                             <TableCell>
-                                <Button color="warning" size="sm" >
-                                    Xem chi tiết
-                                </Button>
-                                <Button color="warning" size="sm" >
-                                    Sửa
-                                </Button>
-                                <Button color="warning" size="sm" >
-                                    Xoá
-                                </Button>
+                                <div className="flex justify-around w-[100px]">
+                                    <div className="flex justify-between">
+                                        <Button
+                                            className="mx-1 rounded-full p-2 hover:bg-yellow-200 transition-colors duration-300"
 
+                                            size="sm"
+                                            onClick={() => onViewService(service)}
+                                            isIconOnly color="danger" aria-label="Like"
+                                        >
+                                            <FaEye />
+                                        </Button>
+                                        <Button
+                                            className="mx-1 rounded-full p-2 hover:bg-blue-200 transition-colors duration-300"
+
+                                            size="sm"
+                                            onClick={() => onUpdateService(service)}
+                                            isIconOnly color="success" aria-label="Like"
+
+                                        >
+                                            <FaEdit />
+                                        </Button>
+
+                                        <Button
+                                            className="mx-1 rounded-full p-2 hover:bg-red-200 transition-colors duration-300"
+                                            isIconOnly color="default" aria-label="Like"
+                                            size="sm"
+                                            onClick={() => onDeleteService(service)}
+
+
+                                        >
+                                            <FaTrash />
+                                        </Button>
+                                    </div>
+                                </div>
                             </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
-
         </div>
     );
 }
