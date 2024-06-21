15e9d1f194b8f172b70299d0b1098896f87ce481
ThienAnDy <vaper2212@gmail.com>
2024-06-21 20:44:09 +0700

    big update

--- src/app/customer/managePet/page.tsx
+++ src/app/customer/managePet/page.tsx
@@ -1,22 +1,44 @@
 "use client";
-import { Button, Input } from "@nextui-org/react";
-import Link from "next/link";
-import React from "react";
-import { AddUser } from "@/components/createUser/page";
-import { TablePet } from "@/components/tablePet/page";
-import { BiUser } from "react-icons/bi";
-import { MdHouse } from "react-icons/md";
-import CreatePet from "../createPet/page";
-
-
-
+import { Button, Input, Tooltip } from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
+import CreatePet from "@/components/createPet/page";
+import getAccessAndRefreshCookie from "@/utilities/authUtils/getCookieForValidation";
+import { FaEye } from "react-icons/fa";
+import { MdChangeCircle, MdDelete } from "react-icons/md";
+import { useAppDispatch } from "@/lib/redux/store";
+import { allPetPaginationData } from "@/models/userModels";
+import { fetchAllPetPagination } from "@/lib/redux/slice/userSlice";
+import PetDetail from "@/components/petDetails/page";
 
 export default function ManageAccount() {
+    const dispatch = useAppDispatch();
+    const [pets, setPets] = useState<allPetPaginationData[]>([]);
+    useEffect(() => {
+        const allPet = async () => {
+            const response = await dispatch(fetchAllPetPagination());
+            setPets(response.payload);
+        }
+        allPet();
+    }, [dispatch]);
+    console.log(pets)
+    const [userId, setUserId] = useState<string>('');
+    useEffect(() => {
+        const fetchUid = async () => {
+            try {
+                const { uid } = await getAccessAndRefreshCookie();
+                if (uid) {
+                    setUserId(uid);
+                }
+            } catch (error) {
+                console.error('Error fetching UID:', error);
+            }
+        };
+        fetchUid();
+    }, [userId]);
+
     return (
-        <div className=" lg:px-6 max-w-[95rem] mx-auto w-full flex flex-col gap-4">
-            <h3 className="text-3xl font-semibold text-center mt-8 mb-4">
-                Danh sách thú cưng
-            </h3>
+        <div className=" lg:px-6 py-5 max-w-[95rem] mx-auto w-full flex flex-col gap-4">
 
             <div className="flex justify-between flex-wrap gap-4 items-center">
                 <div className="flex items-center gap-3 flex-wrap md:flex-nowrap">
@@ -30,14 +52,54 @@ export default function ManageAccount() {
 
                 </div>
                 <div className="flex flex-row gap-3.5 flex-wrap">
-                    <CreatePet />
+                    <CreatePet userId={userId} />
                 </div>
-                {/* <div className="max-w-[95rem] mx-auto w-full">
-                    <ViewDetailAccount />
-                </div> */}
             </div>
             <div className="max-w-[95rem] mx-auto w-full">
-                <TablePet />
+                <Table aria-label="Example static collection table">
+                    <TableHeader>
+                        <TableColumn>Tên thú cưng</TableColumn>
+                        <TableColumn>Loại thú cưng</TableColumn>
+                        <TableColumn>Giới tính</TableColumn>
+                        <TableColumn>Hành động</TableColumn>
+                    </TableHeader>
+                    <TableBody>
+                        {pets.map((pet) => (
+                            <TableRow key={pet.id}>
+                                <TableCell>{pet.petName}</TableCell>
+                                <TableCell>{pet.petType}</TableCell>
+                                <TableCell>{pet.petGender}</TableCell>
+                                <TableCell>
+                                    <div className="flex items-center gap-4 ">
+                                        <div>
+                                            <PetDetail params={pet.id} />
+                                        </div>
+                                        <div>
+                                            <Tooltip content="Chỉnh sửa thú cưng">
+                                                <button onClick={() => console.log("Chỉnh sửa thú cưng")}>
+                                                    <MdChangeCircle size={20} fill="#979797" />
+                                                </button>
+                                            </Tooltip>
+                                        </div>
+                                        <div>
+                                            <Tooltip
+                                                content="Xoá thú cưng"
+                                                color="danger"
+                                                onClick={() => console.log("Xoá thú cưng")}
+                                            >
+                                                <button>
+                                                    <MdDelete size={20} fill="#FF0080" />
+                                                </button>
+                                            </Tooltip>
+                                        </div>
+                                    </div>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+
+
+                    </TableBody>
+                </Table>
             </div>
         </div>
     );
---
+++ src/components/createPet/page.tsx
@@ -0,0 +1,185 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+import {
+    Button,
+    Card,
+    Checkbox,
+    Image,
+    Input,
+    Select,
+    SelectItem,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    useDisclosure,
+    Textarea,
+} from '@nextui-org/react';
+import { ClipLoader } from 'react-spinners';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useAppDispatch } from '@/lib/redux/store';
+import { createPetInput } from '@/models/userModels';
+import { createPet } from '@/lib/redux/slice/userSlice';
+import { FaPlus } from 'react-icons/fa';
+
+export default function CreatePet({ userId }: { userId: string }) {
+    console.log(userId);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+        if (userId) {
+            setPetData(prevData => ({
+                ...prevData,
+                userId: userId,
+            }));
+        }
+    }, [userId]);
+    const [petData, setPetData] = useState<createPetInput>({
+        userId: userId,
+        petName: '',
+        petType: '',
+        petAge: 0,
+        petGender: '',
+        petWeight: 0,
+        petDescription: '',
+        petPhoto: '',
+        petNote: '',
+
+    });
+    const dispatch = useAppDispatch();
+
+    const handleInputChange = (fieldName: string, newValue: string | number) => {
+        setPetData(prevData => ({
+            ...prevData,
+            [fieldName]: newValue
+        }));
+    };
+    const handleCreate = async () => {
+        try {
+            if (userId) {
+                await dispatch(createPet({ petData })).unwrap();
+                toast.success("Tạo thú cưng thành công!", {
+                    onClose: onClose,
+                    autoClose: 1500,
+                });
+            }
+        } catch (error) {
+            console.error('Error creating service:', error);
+            toast.error("Đã xảy ra lỗi khi tạo thú cưng. Vui lòng thử lại sau!");
+        }
+    };
+
+    return (
+        <div>
+            <Button
+                startContent={<FaPlus className='w-5 x' />}
+                onPress={onOpen} className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg ">
+                Tạo mới thú cưng
+            </Button>
+
+            <Modal size='xl' isOpen={isOpen} onClose={onClose} placement="top-center">
+                <ModalContent>
+                    <ModalHeader className='text-3xl text-orange-600'>Tạo mới thú cưng</ModalHeader>
+                    <ModalBody>
+                        <div className="mt-2 flex">
+                            <div className="">
+                                <div className="flex w-full mb-4">
+                                    <div className="mr-4">
+                                        <Input
+                                            type="Petname"
+                                            onChange={(e) => handleInputChange('petName', e.target.value)}
+                                            label="Tên thú cưng"
+                                            className="w-[250px]"
+                                        />
+                                    </div>
+                                    <div className="ml-4">
+                                        <Select label="Loại thú cưng" className="w-[250px]"
+                                            onChange={(e) => handleInputChange('petType', e.target.value)}
+                                        >
+                                            <SelectItem key="DOG">Chó</SelectItem>
+                                            <SelectItem key="CAT">Mèo</SelectItem>
+                                        </Select>
+                                    </div>
+                                </div>
+                                <div className="flex w-full mb-4">
+                                    <div className="mr-4">
+                                        <Input
+                                            onChange={(e) => handleInputChange('petAge', e.target.value)}
+                                            type="Petage"
+                                            label="Tuổi thú cưng"
+                                            className="w-[250px]"
+                                        />
+                                    </div>
+                                    <div className="ml-4">
+                                        <Select label="Giới tính"
+                                            onChange={(e) => handleInputChange('petGender', e.target.value)}
+                                            className="w-[250px]">
+                                            <SelectItem key="Male">Đực</SelectItem>
+                                            <SelectItem key="Female">Cái</SelectItem>
+                                        </Select>
+                                    </div>
+                                </div>
+                                <div className="flex mb-4">
+                                    <div className="w-full mr-4">
+                                        <Input
+                                            onChange={(e) => handleInputChange('petWeight', e.target.value)}
+                                            type="Petweight"
+                                            label="Cân nặng"
+                                            className="w-full"
+                                        />
+
+                                    </div>
+                                    <div className="w-full ml-4">
+                                        <Input
+                                            onChange={(e) => handleInputChange('petDescription', e.target.value)}
+                                            type="petDescription"
+                                            label="Mô tả"
+                                            className="w-full"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="flex mb-4">
+                                    <div className="w-full">
+                                        <Textarea
+                                            onChange={(e) => handleInputChange('petNote', e.target.value)}
+                                            type="petNote"
+                                            label="Ghi chú"
+                                            className="w-full"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </ModalBody>
+                    <ModalFooter>
+                        <div className="mt-4 flex w-full justify-end">
+                            <Button
+                                className=" mr-3 w-full"
+                                onPress={onClose}
+                            >
+                                Huỷ
+                            </Button>
+                            <Button
+                                type="submit"
+                                className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg w-full"
+                                disabled={isLoading}
+                                onPress={handleCreate}
+                            >
+                                {isLoading ? (
+                                    <ClipLoader size={20} color="#ffffff" />
+                                ) : (
+                                    'Tạo mới'
+                                )}
+                            </Button>
+                        </div>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+            <ToastContainer />
+        </div>
+    );
+};
+
+
